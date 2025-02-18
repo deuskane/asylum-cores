7f04c1455ac392895c5e91c05377db72d3420d8b
deuskane <deuskane@gmail.com>
2025-02-18 20:45:31 +0100

    Split import and dump

--- import.sh
+++ import.sh
@@ -15,6 +15,7 @@
 # Date        Version  Author   Description
 # 2021-10-28  1.0      mrosiere Created
 # 2021-11-23  1.1      mrosiere Use getops
+# 2025-02-18  1.2      mrosiere Split dump and import scripts
 #-----------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------
@@ -30,55 +31,6 @@ function import_usage()
     grep " .)\ ##" $0
 
     echo ""
-}
-
-#-----------------------------------------------------------------------------
-# import_main
-#-----------------------------------------------------------------------------
-# 
-#-----------------------------------------------------------------------------
-function import_dump()
-{
-    src_dir=$1
-    file=$2
-
-    rm -f ${file}
-    
-    company_prev=""
-    type_prev=""
-    
-    for core in `find ${src_dir} -iname "*.core" | sort -V`;
-    do
-	
-        fullname=$(grep name ${core}|head -n1)
-        description=$(grep description ${core}|head -n1|sed 's/^[^:]*://g')
-        
-        company=$(echo ${fullname}|cut -d':' -f2| tr -cd [:graph:])
-        type=$(   echo ${fullname}|cut -d':' -f3| tr -cd [:graph:])
-        name=$(   echo ${fullname}|cut -d':' -f4| tr -cd [:graph:])
-        version=$(echo ${fullname}|cut -d':' -f5| tr -cd [:graph:])
-
-	if test "${company}" != "${company_prev}";
-	then
-	    printf "# ${company}\n" >> ${file}
-	    type_prev=""
-	fi;
-
-	if test "${type}" != "${type_prev}";
-	then
-	    printf "## ${type}\n" >> ${file}
-
-	    printf "| %-20s | %-20s | %-20s | %-100s | Description |\n" "Company" "Type" "Name" "Version" >> ${file}
-	    printf "| %-20s | %-20s | %-20s | %-100s | --- |\n"         "---"     "---"  "---"  "---"     >> ${file}
-
-	fi;
-
-	printf "| %-20s | %-20s | %-20s | %-100s | %s|\n" "${company}" "${type}" "${name}" "[${version}](${core/${src_dir}\//})" "${description}">> ${file}
-	
-	company_prev=${company}
-	type_prev=${type}
-
-    done
 }
 
 #-----------------------------------------------------------------------------
@@ -139,6 +91,13 @@ function import_main()
         return;
     fi;
 
+    if test ! -f ${dst_dir}/dump.sh;
+    then
+        echo "[ERROR  ] File \"${dst_dir}/dump.sh\" not found";
+        import_usage $0
+        return;
+    fi;
+    
     echo "[INFO   ] Find core files"
 
     src_cores=`find ${src_dir} -name "*.core"`
@@ -256,8 +215,8 @@ function import_main()
         return;
     fi;
 
-    import_dump ${dst_dir} ${dst_dir}/README.md
-
+    
+    ${dst_dir}/dump.sh
 }
 
 import_main $*
